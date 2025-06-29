6e381a73ceafaa145262d42ab80e4be7f71a369b
juanlucas36041 <juanlucas6309@gmail.com>
2025-06-29 15:10:08 -0300

    Update Jogo-Prototipos.h

--- Jogo/Jogo-Prototipos.h
+++ Jogo/Jogo-Prototipos.h
@@ -1,1 +1,13 @@
+struct carta { // caracteriristicas da carta
+	char nome[30];
+	unsigned atk, def, magia, estrelas;
+};
+struct pack {
+	char nome[8];
+	unsigned short Quant;
+};
 
+int verificador(const char * file, carta Cards[]);
+void tracos(int quant);
+int aleatorio(int sizeX);
+unsigned short menu();
