5b9159b9e91b4de78a6c894828b8e77c16ca8ff5
juanlucas36041 <juanlucas6309@gmail.com>
2025-06-29 15:05:03 -0300

    Update Gerador-Prototipos.h

--- Gerador/Gerador-Prototipos.h
+++ Gerador/Gerador-Prototipos.h
@@ -1,1 +1,14 @@
+struct carta { // caracteriristicas da carta
+	char nome[30];
+	unsigned atk, def, magia, estrelas;
+};
+void tracos(int x);
 
+//funções 
+int verificador(const char * file, carta Cards[]);
+int cadastrar(carta yugioh, carta * cards, int QuantCards);
+void lista(carta * cartas, int QuantCards);
+void alterar(carta * YugiohCards, int NumCard);
+int excluir(carta * YugiohCards, int opcao, int QuantCards);
+int IMPORTAR(const char * fileTxt, carta cards[], int QuantCards);
+void saveGame(const char * fileBin, carta * YugiohCards, int QuantCards);
